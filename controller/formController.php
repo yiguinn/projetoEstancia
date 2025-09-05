d13e92fd53b2b4a128a3caa6132748dcb21fab2f
dudsbtw <eduardo.f.frois.s@gmail.com>
2025-09-05 00:35:50 -0300

    alert de solicitação enviada

--- controller/formController.php
+++ controller/formController.php
@@ -1,26 +1,23 @@
 <?php
-    require_once '../model/formModel.php';
-    
-    $conn = new formModel();
+require_once '../model/formModel.php';
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $nome = $_POST['nometxt'];
-        $telefone = $_POST['telefonenum'];
-        $email = $_POST['emailtxt'];
-        $evento = $_POST['eventotxt'];
-        $data = $_POST['data_preferencial'];
-        $convidados = $_POST['numero_convidados'];
-        $mensagem = $_POST['mensagemtxt'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $model = new formModel();
 
-        $conn->inserir($nome, $telefone, $email, $evento, $data, $convidados, $mensagem);
-        
-        print_r($nome);
-        print_r($telefone);
-        print_r($email);
-        print_r($evento);
-        print_r($data);
-        print_r($convidados);
-        print_r($mensagem);
-        exit();
+    $sucesso = $model->inserir(
+        $_POST['nometxt'],
+        $_POST['telefonenum'],
+        $_POST['emailtxt'],
+        $_POST['eventotxt'],
+        $_POST['data_preferencial'], 
+        $_POST['numero_convidados'],
+        $_POST['mensagemtxt']
+    );
+
+    if ($sucesso) {
+        echo "<script>alert('Sua solicitação foi enviada!'); window.location.href = '../view/index.php';</script>"; 
+    } else {
+        echo "erro";
     }
+}
 ?>
