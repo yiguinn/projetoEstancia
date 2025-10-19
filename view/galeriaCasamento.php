e40b0d3a5bc738c1eff6251cc8b61d7e81e6e34f
remynzo <miranda.rebelloe@gmail.com>
2025-10-19 17:17:08 -0300

    teste correcao estilo galeria

--- view/galeriaCasamento.php
+++ view/galeriaCasamento.php
@@ -11,10 +11,6 @@ $imagens = $model->listarPorCategoria($categoria);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Galeria de Casamento - Estância Ilha da Madeira</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link href="https://fonts.googleapis.com/css2?family=SF+Pro+Display:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="view/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 </head>
 <?php include_once ("header.php")?>
 <body class="bg-white">
