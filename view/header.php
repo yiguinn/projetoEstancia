184e47e367525845a20ccb041eea7291f21a9013
remynzo <miranda.rebelloe@gmail.com>
2025-10-19 17:46:12 -0300

    teste

--- view/header.php
+++ view/header.php
@@ -13,7 +13,7 @@ $is_admin = $is_logged_in && $_SESSION['user_role'] === 'admin';
     <title>Estancia Ilha da Madeira - Casamentos</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=SF+Pro+Display:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="/view/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     
     <style>
