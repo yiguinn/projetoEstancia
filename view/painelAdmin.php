5ed7ea1f1246af1dec6f9400c12a8a4768405ca5
remynzo <miranda.rebelloe@gmail.com>
2025-10-19 21:01:37 -0300

    att

--- view/painelAdmin.php
+++ view/painelAdmin.php
@@ -13,7 +13,6 @@ if (!isset($_SESSION['user_id']) || $_SESSION['user_role'] !== 'admin') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Painel Administrativo - Estância</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="view/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 </head>
 <body class="bg-gray-100">
