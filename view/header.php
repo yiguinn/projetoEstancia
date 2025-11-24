7b214788a6894c3134c19f8531bd4da207d527e6
felipe1men <felipe.mendonca.lima@gmail.com>
2025-11-24 00:23:01 -0300

    hm grrrrr

--- view/header.php
+++ view/header.php
@@ -18,7 +18,7 @@ $is_admin = $is_logged_in && isset($_SESSION['user_role']) && $_SESSION['user_ro
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=SF+Pro+Display:wght@400;500;600;700&display=swap" rel="stylesheet">
     
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../view/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <link rel="icon" type="image/png" href="imagens/logo.png">
     
@@ -45,8 +45,8 @@ $is_admin = $is_logged_in && isset($_SESSION['user_role']) && $_SESSION['user_ro
                 <i class="fas fa-universal-access fa-xl"></i>
             </button>
 
-            <a href="../index.php" class="flex items-center space-x-3 pl-12">
-                <img src="imagens/logo.png" width="50" alt="Logo">
+            <a href="../view/index.php" class="flex items-center space-x-3 pl-12">
+                <img src="../view/imagens/logo.png" width="50" alt="Logo">
                 <div>
                     <h1 class="text-xl font-medium text-rosa-vibrante">Estância</h1>
                     <p class="text-sm text-gray-600 -mt-1">Ilha da Madeira</p>
